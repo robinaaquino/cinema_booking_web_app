a4aedd5c674f3e91ce17a79dd5e2705b70ba822c
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-03-02 15:34:25 +0800

    db: ran db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_072554) do
+  create_table "assignments", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "cinema_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cinema_id"], name: "index_assignments_on_cinema_id"
+    t.index ["movie_id"], name: "index_assignments_on_movie_id"
+  end
+
+  create_table "cinemas", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cinemas_on_user_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
+
+  create_table "seats", force: :cascade do |t|
+    t.integer "timeslot"
+    t.integer "seat_number"
+    t.integer "user_id", null: false
+    t.integer "cinema_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cinema_id"], name: "index_seats_on_cinema_id"
+    t.index ["user_id"], name: "index_seats_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "number"
+    t.string "string"
+    t.string "password_digest"
+    t.integer "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "assignments", "cinemas"
+  add_foreign_key "assignments", "movies"
+  add_foreign_key "cinemas", "users"
+  add_foreign_key "movies", "users"
+  add_foreign_key "seats", "cinemas"
+  add_foreign_key "seats", "users"
+end

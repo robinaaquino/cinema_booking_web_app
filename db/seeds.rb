c9da1dcfbd0ab2023249a90a5759c3cb3fcee346
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-03-03 03:18:43 +0800

    feat: added seeded data

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+User.create!([{
+  full_name: 'user', email: 'user@user.com', number: '123', password: 'testtest', role: 'customer'
+},
+{
+  full_name: 'admin', email: 'admin@user.com', number: '123', password: 'testtest', role: 'admin'
+}])

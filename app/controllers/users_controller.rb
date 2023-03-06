dd9c906ad3b1f69d7aded6d02f40b2c9a70301cb
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-03-06 21:30:16 +0800

    feat: added check for no movies and cleaned up user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :admin_user, only: [:index, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :admin_user, only: [:index]
 
   def index
     @users = User.all
